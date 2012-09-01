5b2c38350c473eaf4b3390339607547b03d9471a
Kai Gittens <kai.gittens@gmail.com>
2012-09-01 06:38:56 -0400

    updated scripts.js, updated jQuery to version 1.8.1

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,33 @@
-(function(){ //load our JS inside of a self-executing JavaScript function
+(function(){
 
-//Keep all of our variables in a single var pattern -> http://bit.ly/oNH8Q6
 var findDiv, copyDiv, insideBox,
-    wrapper = document.createDocumentFragment(), //document fragment that we'll load our elements into
-    newBox = document.createElement('div'), //create a div tag, which is the element that will hold all the other elements
-    boxHeader = document.createElement('h1'), //create an h1 tag
-    newInnerBox = document.createElement('div'), //create another div tag
-    copy = document.createTextNode('Hello, World!'), //create a text string
-    mainNode = document.getElementById('container'); //reference a div tag that's already on 'index.html'
+    wrapper = document.createDocumentFragment(),
+    outerDiv = document.createElement('div'),
+    boxHeader = document.createElement('h1'),
+    innerDiv = document.createElement('div'),
+    copy = document.createTextNode('Hello, World!'),
+    mainNode = document.getElementById('container');
 
-newBox.appendChild(boxHeader); //insert the 'boxHeader' h1 tag into the 'newBox' div
-newBox.appendChild(newInnerBox); //insert the 'newInnerBox' div into the newBox' div
-newInnerBox.appendChild(copy); //insert the 'copy' text string into the newInnerBox' div
-boxHeader.innerHTML = 'The Original Box'; //our h1 tag is empty...let's load some copy into it
-$(newBox).attr('id', 'outerBoxOne'); //use jQuery to add an id of 'outerBoxOne' to our 'newBox' div
-$(newInnerBox).attr('id', 'innerBoxOne'); //use jQuery to add an id of 'innerBoxOne' to our 'newInnerBox' div
-wrapper.appendChild(newBox); //insert 'newBox' into our document fragment: 'newBox' contains all the elements now
-mainNode.appendChild(wrapper); //insert the document fragment into our div that's already on the page
+boxHeader.innerHTML = 'The Original Box';
+outerDiv.appendChild(boxHeader);
+outerDiv.appendChild(innerDiv);
+innerDiv.appendChild(copy);
+$(outerDiv).attr('id', 'outerBoxOne');
+$(innerDiv).attr('id', 'innerBoxOne');
+wrapper.appendChild(outerDiv);
+mainNode.appendChild(wrapper);
 
-findDiv = document.getElementById('outerBoxOne');  //find the div on the page that contains all the elements
-copyDiv = findDiv.cloneNode(true); //copy the div using cloneNode()
+findDiv = document.getElementById('outerBoxOne');
+copyDiv = findDiv.cloneNode(true);
 copyDiv.childNodes[0].innerHTML = 'The Cloned Box';
 insideBox = copyDiv.childNodes[1];
 insideBox.innerHTML = 'Goodnight, Moon!';
-$(copyDiv).attr('id', 'outerBoxTwo'); //use jQuery to add an id of 'outerBoxTwo' to our 'copyDiv' div
+$(copyDiv).attr('id', 'outerBoxTwo');
 $(insideBox).attr({
     id: 'innerBoxTwo',
     'class': 'blueBg'
-});
+ });
 
-mainNode.appendChild(copyDiv); //insert 'the document fragment 'copyDiv' into our div that's already on the page
+mainNode.appendChild(copyDiv);
 
 }());
